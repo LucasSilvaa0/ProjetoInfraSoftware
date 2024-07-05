b1d7273d04b5b5e6d6f0da3493c6bdc555dc295b
Welton Felix <contato.weltonf@gmail.com>
2024-07-05 11:26:04 -0300

    Adiciona tempo entre grupos de clientes

--- city_barber/src/Main.java
+++ city_barber/src/Main.java
@@ -1,24 +1,25 @@
+import java.util.Objects;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         Barbershop barbershop = new Barbershop();
 
-        Thread clients[] = new Thread[15]; // Criando espaço de memória para salvar as threads
-        for (int i = 1; i <= 15; i++) {
+        Thread clients[] = new Thread[20]; // Criando espaço de memória para salvar as threads
+        for (int i = 1; i <= 20; i++) {
             clients[i-1] = new Thread(new Client(barbershop, i)); // Criando os clientes
         }
 
-        for (int i = 0; i < 15; i++) {
-            clients[i].start(); // Dando start em todas as threads criadas
-
-            if (Object.equals(i, 8) || Object.equals(i, 20)) {
+        for (int i = 0; i < 20; i++) {
+            if (Objects.equals(i, 8) || Objects.equals(i, 14)) {
                 try {
-                    Thread.sleep(4000);
+                    Thread.sleep(2000);
                 } catch (InterruptedException e) {
                     throw new RuntimeException();
                 }
             }
+            clients[i].start(); // Dando start em todas as threads criadas
         }
     }
 }
